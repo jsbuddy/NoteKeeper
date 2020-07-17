5e4b6dd9d65b4d71ad68693d05fa65df32c7640b
jsbuddy <judecodes@gmail.com>
2020-07-17 15:27:37 +0100

    create automated ui test for navigation

---
+++ app/src/androidTest/java/com/example/notekeeper/NavigationTest.kt
@@ -0,0 +1,50 @@
+package com.example.notekeeper
+
+import android.view.Gravity
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.*
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+    @get:Rule
+    val itemsActivity = ActivityTestRule(ItemsActivity::class.java)
+
+    @Test
+    fun selectNoteAfterNavigationDrawerChange() {
+        onView(withId(R.id.drawer_layout))
+            .check(matches(isClosed(Gravity.LEFT)))
+            .perform(DrawerActions.open())
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_courses))
+
+        onView(withId(R.id.listCourses)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<CourseRecyclerAdapter.ViewHolder>(0, click())
+        )
+
+        onView(withId(R.id.drawer_layout))
+            .check(matches(isClosed(Gravity.LEFT)))
+            .perform(DrawerActions.open())
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes))
+
+        onView(withId(R.id.listItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<NoteRecyclerAdapter.ViewHolder>(0, click())
+        )
+
+        val note = DataManager.notes[0]
+        onView(withId(R.id.spinnerCourses)).check(matches(withSpinnerText(note.course?.title)))
+        onView(withId(R.id.textNoteTitle)).check(matches(withText(note.title)))
+        onView(withId(R.id.textNoteText)).check(matches(withText(note.text)))
+    }
+}
